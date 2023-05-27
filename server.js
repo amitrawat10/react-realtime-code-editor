56586eadbb347fafbb8f3b9b9969d61bdf36f0a4
Amit Rawat <amitrawat1099@gmail.com>
2023-05-27 15:43:16 +0530

    bulid generated

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const SOCKET_EVENTS = require("./src/socketActions");
+const path = require("path");
 const server = http.createServer(app);
+
+app.use(express.static("build"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
